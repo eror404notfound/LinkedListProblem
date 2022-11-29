963081390c6f6f2a96d2117c8d96d9e8179916d5
Ankit <ankitcool4370@gmail.com>
2022-11-29 14:55:28 +0530

    Welcome to the Link List

---
+++ LinkedListUCs/Linkedlist.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LinkedListUCs
+{
+    public class LinkedList
+    {
+        internal Node head;
+        internal void Addlast(int data)
+        {
+            Node newNode = new Node(data); // creating the new node Passing the data
+
+            if (this.head == null) // if this is null than it will start the new node.
+            {
+                this.head = newNode;
+
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null) // if this note is not equal to null that it will print the data.
+                {
+                    temp = temp.next;
+                }
+                temp.next = newNode;
+            }
+            Console.WriteLine("{0} is inserted in to Linked list", newNode.data);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Display of Nodes");
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is Empty");
+                return;
+            }
+            else
+            {
+                while (temp != null)
+                {
+                    Console.Write("" + temp.data + " ");
+                    temp = temp.next;
+
+                }
+            }
+        }
+    }
+}
